79d9dfca5ccf38aa2fef31d2ec4c77218f65429a
Ádám Knapecz <knapecz.adam@gmail.com>
2023-12-13 23:25:19 +0100

    Alternative fix for failed assertion: unknown issue

--- ImGui/src/imgui.cpp
+++ ImGui/src/imgui.cpp
@@ -9419,7 +9419,12 @@ static void ImGui::ErrorCheckNewFrameSanityChecks()
     // Check user data
     // (We pass an error message in the assert expression to make it visible to programmers who are not using a debugger, as most assert handlers display their argument)
     IM_ASSERT(g.Initialized);
-    IM_ASSERT((g.IO.DeltaTime > 0.0f || g.FrameCount == 0)              && "Need a positive DeltaTime!");
+    //------------------------------------------------------------------------------------------------------------------------
+    // TODO: Assertion failed
+    // IM_ASSERT((g.IO.DeltaTime > 0.0f || g.FrameCount == 0)              && "Need a positive DeltaTime!");
+    // https://github.com/ocornut/imgui/issues/4680
+    if (g.IO.DeltaTime <= 0.0f) g.IO.DeltaTime = 0.00001f;
+    //------------------------------------------------------------------------------------------------------------------------
     IM_ASSERT((g.FrameCount == 0 || g.FrameCountEnded == g.FrameCount)  && "Forgot to call Render() or EndFrame() at the end of the previous frame?");
     IM_ASSERT(g.IO.DisplaySize.x >= 0.0f && g.IO.DisplaySize.y >= 0.0f  && "Invalid DisplaySize value!");
     IM_ASSERT(g.IO.Fonts->IsBuilt()                                     && "Font Atlas not built! Make sure you called ImGui_ImplXXXX_NewFrame() function for renderer backend, which should call io.Fonts->GetTexDataAsRGBA32() / GetTexDataAsAlpha8()");
